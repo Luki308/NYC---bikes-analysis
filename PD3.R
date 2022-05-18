1b88939dda9a6190bfc57168e639d4ee3c008ac0
Luki308 <01159348@pw.edu.pl>
2022-05-18 22:23:34 +0200

    Male zmiany

--- PD3.R
+++ PD3.R
@@ -1,2 +1,28 @@
-Tabela <- read.csv("JC-202204-citibike-tripdata.csv/JC-202204-citibike-tripdata.csv")
-Pogoda <- read.csv("pogoda-2016/weather_data_nyc_centralpark_2016(1).csv")
+library(dplyr)
+
+m19 <- read.csv("data/marce/201903-citibike-tripdata.csv")
+m20 <- read.csv("data/marce/202003-citibike-tripdata.csv")
+m22 <- read.csv("data/marce/202203-citibike-tripdata.csv")
+
+m19 <- as.data.frame(m19)
+m20 <- as.data.frame(m20)
+m22 <- as.data.frame(m22)
+
+
+m20 %>%
+  select(starttime,tripduration) %>%
+  mutate(starttime = substr(starttime,1,10)) %>%
+  group_by(starttime) %>%
+  summarise(Count = n(), Avg = mean(tripduration/60)) -> wynik20
+
+m19 %>%
+  select(starttime,tripduration) %>%
+  mutate(starttime = substr(starttime,1,10)) %>%
+  group_by(starttime) %>%
+  summarise(Count = n(), Avg = mean(tripduration/60)) -> wynik19
+
+m20 %>%
+  select(starttime,tripduration) %>%
+  mutate(starttime = substr(starttime,1,10)) %>%
+  group_by(starttime) %>%
+  summarise(Count = n(), Avg = mean(tripduration/60)) -> wynik22
