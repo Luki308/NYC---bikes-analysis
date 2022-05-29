805fefde7ef28549912de77acbbd28cd5e183e66
Luki308 <01159348@pw.edu.pl>
2022-05-29 02:14:54 +0200

    Zakomentowanie kodu, zrobienie porzadkow, dodanie 2 kolejnych map

--- Leaflet_map.R
+++ Leaflet_map.R
@@ -4,10 +4,13 @@ library(dplyr)
 library(sp)
 library(tigris) #geo_join
 
-load("Dane_lokalizacyjne.RData")
+load("Stacje_z_osiedlami.RData")
 
+
+######### 1
 plot_data <- geo_join(nyc_neighborhoods, ilosc_w_neighborhood,
-                      "neighborhood", "neighborhood")
+                      "neighborhood", "neighborhood",
+                      how = 'inner')
 
 pal <- colorNumeric(palette = heat.colors(10,rev = T),
                     domain = range(plot_data@data$Count, na.rm=T))
@@ -15,14 +18,51 @@ pal <- colorNumeric(palette = heat.colors(10,rev = T),
 leaflet(plot_data) %>%
   addTiles() %>%
   addPolygons(color = "black", weight = 1 , opacity = 1,
-              fillColor = ~pal(Count), fillOpacity = 0.7,
+              fillColor = ~pal(Count), fillOpacity = 0.9,
               popup = ~neighborhood) %>%
   addLegend("bottomright", pal = pal, values = ~Count,
-            title = "Liczba stacji w osiedlach",
+            title = "Liczba stacji na osiedlach",
             opacity = 1
-  )
-  setView(-74.00, 40.71, zoom = 12) %>%
-  addProviderTiles
-?addPolygons
-?colorNumeric
-  
+  ) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  setView(-74.00, 40.71, zoom = 12)
+
+
+######### 2
+plot_data2 <-  geo_join(nyc_neighborhoods, start_station,
+                        "neighborhood", "neighborhood",
+                        how = 'inner')
+pal2 <- colorNumeric(palette = heat.colors(10,rev = T),
+                    domain = range(plot_data2@data$Count, na.rm=T))
+
+leaflet(plot_data2) %>%
+  addTiles() %>%
+  addPolygons(color = "black", weight = 1 , opacity = 1,
+              fillColor = ~pal2(Count), fillOpacity = 0.9,
+              popup = ~neighborhood) %>%
+  addLegend("bottomright", pal = pal2, values = ~Count,
+            title = "Liczba przejazdow (Start) na osiedlach",
+            opacity = 1
+  ) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  setView(-74.00, 40.71, zoom = 12)
+
+
+######### 3
+plot_data3 <-  geo_join(nyc_neighborhoods, end_station,
+                        "neighborhood", "neighborhood",
+                        how = 'inner')
+pal3 <- colorNumeric(palette = heat.colors(10,rev = T),
+                     domain = range(plot_data3@data$Count, na.rm=T))
+
+leaflet(plot_data3) %>%
+  addTiles() %>%
+  addPolygons(color = "black", weight = 1 , opacity = 1,
+              fillColor = ~pal3(Count), fillOpacity = 0.9,
+              popup = ~neighborhood) %>%
+  addLegend("bottomright", pal = pal3, values = ~Count,
+            title = "Liczba przejazdow (Koniec) na osiedlach",
+            opacity = 1
+  ) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  setView(-74.00, 40.71, zoom = 12)
---
+++ Stacje_nbh.R
@@ -0,0 +1,69 @@
+# Mapa pokazująca zagęszczenie korzystania ze stacji rowerowych w poszczególnych
+# osiedlach NYC
+#https://rpubs.com/jhofman/nycmaps
+library(ggplot2)
+library(dplyr)
+library(httr) #GET geojson
+library(rgdal) #readOGR - przekształca geojson na Spatial Data Frame; także sp
+library(broom) #tidy - spatial Data Frame -> tibble = data.frame
+library(sp)
+
+k22 <- read.csv("data/lokalizacja/202204-citibike-tripdata.csv")
+k22 <- as.data.frame(k22)
+
+k22 %>%
+  select(ID = start_station_id, Name = start_station_name, Lat = start_lat, Lng = start_lng) %>%
+  distinct(ID,Name,Lat,Lng) -> stacje
+
+r <- GET('http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson')
+nyc_neighborhoods <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
+
+nyc_neighborhoods_df <- tidy(nyc_neighborhoods) # wielokąty dla każdego osiedla (rozpoznawanego po id)
+
+#Polaczyc stacje z iloscia przejzadow (startowych + konczonych - osobne kolumny)
+
+# Przypisywanie stacjom okolicy (neighborhood)
+points <- stacje[,c(1,3,4)]
+points_spdf <- points
+coordinates(points_spdf) <- ~Lng +  Lat                      #przekształcanie na Spatial df,
+proj4string(points_spdf) <- proj4string(nyc_neighborhoods)   #system zmiany systemu współrzędnych?
+matches <- over(points_spdf, nyc_neighborhoods)              # "nakładanie" punktów na wielokąty (osiedla), przypisywanie stacji osiedlom
+points <- cbind (points, matches)                            # przydzielenie stacjom ich osiedli                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
+# Inaczej:
+# coords <- stacje[,c(3,4)]
+# data <- stacje[,1,drop = FALSE]
+# coordinates(data) = cbind(coords$Lng,coords$Lat)
+# proj4string(data) <- proj4string(nyc_neighborhoods)
+# matches2 <- over(data, nyc_neighborhoods)
+
+
+ilosc_w_neighborhood <- points %>%
+  group_by(neighborhood) %>%
+  summarise(Count = n())
+
+start_station <- k22 %>%
+  group_by(start_station_id) %>%
+  summarise(Count = n())
+
+start_station <- left_join(x = start_station,
+                                y = points,
+                                by = c("start_station_id" = "ID"))
+
+end_station <- k22 %>%
+  group_by(end_station_id) %>%
+  summarise(Count = n())
+
+end_station <- left_join(x = end_station,
+                         y = points,
+                         by = c("end_station_id" = "ID"))
+
+plot_data <- tidy(nyc_neighborhoods, region="neighborhood") %>%
+  left_join(., ilosc_w_neighborhood, by=c("id"="neighborhood")) %>%
+  filter(!is.na(Count))
+
+ggplot() + 
+  geom_polygon(data=plot_data, aes(x=long, y=lat, group=group, color = "red", fill = Count), alpha = 0.75)
+
+
+
